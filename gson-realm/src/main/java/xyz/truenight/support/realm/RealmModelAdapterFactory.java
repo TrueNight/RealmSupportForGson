df002508ae9b36d7467168ab75959e72dc4de7af
TrueNight <twilightinnight@gmail.com>
2017-03-22 19:54:08 +0300

    Updated license.

--- gson-realm/src/main/java/xyz/truenight/support/realm/RealmModelAdapterFactory.java
+++ gson-realm/src/main/java/xyz/truenight/support/realm/RealmModelAdapterFactory.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2017 Mikhail Frolov
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package xyz.truenight.support.realm;
 
 import com.google.gson.Gson;
@@ -11,10 +27,6 @@ import java.io.IOException;
 
 import io.realm.RealmModel;
 import io.realm.RealmObject;
-
-/**
- * Copyright (C) 2017 Mikhail Frolov
- */
 
 class RealmModelAdapterFactory implements TypeAdapterFactory {
 
